89ba8701c52b53ce1eb0e0e0c3f324d70646968d
Daniel Ilário de Oliveira <97920051+Daniel-I-Oliveira@users.noreply.github.com>
2022-07-02 16:06:09 -0300

    Novo arquivo
    Welcome.c

---
+++ Welcome.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+main()
+{
+char name[20];
+printf("Hello! My name is Daniel. What is your name?\n");
+scanf("%s", &name);
+
+printf("Nice to meet you, %s! Welcome to my repository on GitHub!",name);
+}
