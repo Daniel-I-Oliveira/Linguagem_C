bb3b68dcf3fd01d4962f636f35e1fd8d347d3a02
Daniel Ilário de Oliveira <97920051+Daniel-I-Oliveira@users.noreply.github.com>
2022-07-02 16:45:44 -0300

    Novo arquivo
    Exercicio_01

---
+++ Exercicio_01.c
@@ -0,0 +1,20 @@
+// Escreva um programa que leia um número inteiro e apresente seu antecessor e seu sucessor.
+
+#include<stdio.h>
+#include<Windows.h>
+
+main()
+{
+    int numero, antecessor, sucessor;
+
+    SetConsoleOutputCP(CP_UTF8);
+
+    printf("Número: ");
+    scanf("%d", &numero);
+
+    antecessor = numero - 1;
+    sucessor = numero + 1;
+
+    printf("\nO antecessor de %d é: %d", numero, antecessor);
+    printf("\nO sucessor de %d é: %d", numero, sucessor);
+}
